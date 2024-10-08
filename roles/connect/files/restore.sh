47eff8a5f4ac1b8e40ed36c8a5f1e22556497624
Jose Angel de Bustos Perez <jadebustos@gmail.com>
2024-10-08 17:53:28 +0200

    restore

--- roles/connect/files/restore.sh
+++ roles/connect/files/restore.sh
@@ -21,4 +21,18 @@ case "${USER}" in
 	/usr/bin/chmod 0664 /imagemode/image-mode-test.qcow2
 	;;
 
+	"openscap")
+	/usr/bin/sudo /usr/bin/qemu-img convert -f qcow2 -O qcow2 -o lazy_refcounts=on /var/lib/libvirt/images/templates/openscap.qcow2 /var/lib/libvirt/images/openscap.qcow2
+	/usr/bin/logger -p local4.info -t "User: "${USER} "Restoring openscap vm - "$?
+	/usr/bin/sudo /usr/bin/virsh start openscap
+	/usr/bin/logger -p local4.info -t "User: "${USER}"Starting openscap vm - "$?
+	;;
+
+	"podman")
+	/usr/bin/sudo /usr/bin/qemu-img convert -f qcow2 -O qcow2 -o lazy_refcounts=on /var/lib/libvirt/images/templates/podman.qcow2 /var/lib/libvirt/images/podman.qcow2
+	/usr/bin/logger -p local4.info -t "User: "${USER} "Restoring podman vm - "$?
+	/usr/bin/sudo /usr/bin/virsh start podman
+	/usr/bin/logger -p local4.info -t "User: "${USER}"Starting podman vm - "$?
+	;;
+
 esac
